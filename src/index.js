87593fa14cc21725a13e1f4dfd3736a16064ee3a
Archie Lee <archielee@appier.com>
2015-12-29 11:53:50 +0800

    WIP

--- src/index.js
+++ src/index.js
@@ -1,4 +1,62 @@
+import Immutable from 'immutable';
 
-export default function (data, keyPath, updater) {
-  return data;
+export const filterer = (fn) => {
+	return {
+		type: 'filter',
+		fn,
+	}
 }
+
+export const mapperer = (fn) => {
+	return {
+		type: 'mapper',
+		fn,
+	}
+}
+
+function inception(data, keyPath, updater) {
+  let keeper = [(f) => f(data)];
+
+  keyPath.push(updater);
+
+	keyPath.forEach((obj, i) => {
+		if (typeof obj === 'string' || typeof obj === 'number' ) {
+			keeper[i+1] = innerFn => keeper[i](d=>{
+				return d.updateIn([obj], innerFn)
+			})
+		}
+		else if (typeof obj === 'object') {
+			if(obj.type === 'filter'){
+				keeper[i+1] = innerFn => {
+					return keeper[i](d=>{
+            if(Immutable.Map.isMap(d)){
+            	if(obj.fn(d)){
+              	return innerFn(d);
+              }
+              return d;
+            }
+            else if(Immutable.List.isList(d)){
+              return d.map(dd=>{
+                if(obj.fn(dd)){
+                  return innerFn(dd);
+                }
+                return dd;
+               });
+            }
+					})
+				}
+			}
+			else if(obj.type === 'mapper'){
+      	keeper[i+1] = innerFn => {
+					return keeper[i](d=>{
+						return innerFn(obj.fn(d))
+          })
+				}
+			}
+		}
+	});
+
+	return keeper[keyPath.length](d=>d);
+}
+
+export default inception;
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,33 +1,58 @@
 import expect from 'expect';
 import dummy from './fixtures/dummy';
-import inception from 'src/index';
+import inception, { filterer, mapperer } from 'src/index';
 
 describe('Inception', () => {
   it('supports single navigator (string) path', () => {
+    const exp = dummy.set('users', 'overwrite');
+    const act = inception(
+      dummy,
+      ['users'],
+      mapperer(() => 'overwrite')
+    );
+
+    expect(act.toJS()).toEqual(exp.toJS());
+  });
+
+  it('supports navigator (string) + filterer (function) path', () => {
     const exp = dummy.update(
       'users',
       updater => updater.map(item => item.set('age', item.get('age') + 1))
     );
     const act = inception(
       dummy,
-      ['users'],
-      item => item.set('age', item.get('age') + 1)
+      ['users', filterer(() => true)],
+      mapperer(item => item.set('age', item.get('age') + 1))
     );
 
     expect(act.toJS()).toEqual(exp.toJS());
   });
 
-  it('supports single filterer (function) path', () => {
-    const exp = dummy.update(
-      'banks',
-      updater => updater.map(item => item.set('marked', true))
-    );
-    const act = inception(
-      dummy,
-      [items => items.count() > 3],
-      item => item.set('marked', true)
-    );
+  // it('supports single navigator (string) path', () => {
+  //   const exp = dummy.update(
+  //     'users',
+  //     updater => updater.map(item => item.set('age', item.get('age') + 1))
+  //   );
+  //   const act = inception(
+  //     dummy,
+  //     ['users'],
+  //     mapperer(item => item.set('age', item.get('age') + 1))
+  //   );
+  //
+  //   expect(act.toJS()).toEqual(exp.toJS());
+  // });
 
-    expect(act.toJS()).toEqual(exp.toJS());
-  });
+  // it('supports single filterer (function) path', () => {
+  //   const exp = dummy.update(
+  //     'banks',
+  //     updater => updater.map(item => item.set('marked', true))
+  //   );
+  //   const act = inception(
+  //     dummy,
+  //     [filterer(items => items.count() > 3)],
+  //     mapperer(item => item.set('marked', true))
+  //   );
+  //
+  //   expect(act.toJS()).toEqual(exp.toJS());
+  // });
 });
